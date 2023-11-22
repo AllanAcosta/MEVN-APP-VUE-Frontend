05db22b80a08ec03fd651296d955618ff6a9625c
Allan Acosta <46501625+AllanAcosta@users.noreply.github.com>
2023-11-22 10:09:00 -0500

    Project Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,vue}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
+    colors: {
+      'smoky-black': '#090C02',
+      'lapis-lazuli': '#255f85',
+      'cardinal': '#c5283d',
+      'brunt-sienna': '#e9724c'
+
+    }
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
